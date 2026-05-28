4e2ae2f8aadedf87f9deb9059745fb73d5d7cc11
rrreeeaaauuu1-cpu <rrreeeaaauuu1@gmail.com>
2026-05-29 02:26:23 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,50 +1,72 @@
 const satori = require('satori');
 const { Resvg } = require('@resvg/resvg-js');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = async function handler(req, res) {
-  // 1. URL 파라미터 가져오기
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  
-  const getParam = (key, defaultValue) => {
-    const val = url.searchParams.get(key) || defaultValue;
-    return val.replace(/_/g, ' ');
-  };
+  try {
+    const url = new URL(req.url, \`http://\${req.headers.host}\`);
+    
+    const getParam = (key, defaultValue) => {
+      const val = url.searchParams.get(key) || defaultValue;
+      return val.replace(/_/g, ' ');
+    };
 
-  const room = getParam('room', '열람실 알 수 없음'); 
-  const turn = getParam('turn', '0');     
-  const cgt = getParam('cgt', '0');     
-  const attitude = getParam('att', '평온');     
-  const progress = getParam('prog', '0');     
+    const room = getParam('room', '열람실 알 수 없음'); 
+    const turn = getParam('turn', '0');     
+    const cgt = getParam('cgt', '0');     
+    const attitude = getParam('att', '평온');     
+    const progress = getParam('prog', '0');     
 
-  // 2. SVG 생성 (Satori 사용 - 폰트는 기본 폰트 사용 또는 별도 로드 필요)
-  const svg = await satori(
-    {
-      type: 'div',
-      props: {
-        style: { display: 'flex', width: '1024px', height: '512px', backgroundImage: 'url(https://project-episod.com/ink.png)' },
-        children: [
-          // 여기에 글자 위치를 div와 absolute 스타일로 지정해야 해요..! (기존 SVG 문법과 약간 다름)
-          // 시간상 간략화된 예시입니다.
-          { type: 'div', props: { style: { position: 'absolute', top: '106px', left: '285px', fontSize: '30px', color: '#e8d9a8' }, children: room } },
-          { type: 'div', props: { style: { position: 'absolute', top: '220px', left: '280px', fontSize: '26px', color: '#e8d9a8' }, children: turn } },
-        ]
+    // 폰트 파일 읽기
+    const fontPath = path.join(process.cwd(), 'fonts', 'NotoSerifKR-Regular.ttf');
+    const fontData = fs.readFileSync(fontPath);
+
+    const svg = await satori(
+      {
+        type: 'div',
+        props: {
+          style: { 
+            display: 'flex', 
+            width: '1024px', 
+            height: '512px', 
+            backgroundImage: 'url(https://project-episod.com/ink.png)',
+            backgroundSize: '1024px 512px'
+          },
+          children: [
+            { type: 'div', props: { style: { position: 'absolute', top: '106px', left: '285px', fontSize: '30px', color: '#e8d9a8' }, children: room } },
+            { type: 'div', props: { style: { position: 'absolute', top: '220px', left: '280px', fontSize: '26px', color: '#e8d9a8' }, children: turn } },
+            { type: 'div', props: { style: { position: 'absolute', top: '282px', left: '324px', fontSize: '22px', color: '#e8d9a8' }, children: \`\${cgt}%\` } },
+            { type: 'div', props: { style: { position: 'absolute', top: '338px', left: '405px', fontSize: '22px', color: '#e8d9a8' }, children: attitude } },
+            { type: 'div', props: { style: { position: 'absolute', top: '406px', left: '300px', fontSize: '22px', color: '#e8d9a8' }, children: \`열람한 책 [ \${progress} ] 권\` } },
+          ]
+        }
+      },
+      {
+        width: 1024,
+        height: 512,
+        fonts: [
+          {
+            name: 'Noto Serif KR',
+            data: fontData,
+            weight: 400,
+            style: 'normal',
+          },
+        ],
       }
-    },
-    {
-      width: 1024,
-      height: 512,
-      fonts: [] // 주의: Satori는 폰트 파일(.ttf)이 필수적으로 필요해요!
-    }
-  );
+    );
 
-  // 3. SVG를 PNG로 변환
-  const resvg = new Resvg(svg, { fitTo: { mode: 'width', value: 1024 } });
-  const pngData = resvg.render();
-  const pngBuffer = pngData.asPng();
+    const resvg = new Resvg(svg, { fitTo: { mode: 'width', value: 1024 } });
+    const pngData = resvg.render();
+    const pngBuffer = pngData.asPng();
 
-  // 4. PNG 이미지로 응답
-  res.setHeader('Content-Type', 'image/png');
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.send(pngBuffer);
+    res.setHeader('Content-Type', 'image/png');
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(pngBuffer);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 };
